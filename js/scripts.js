b15fdfd2301e55492e8ac963bd7e96e34def16a9
angus-barclay <79063844+angus-barclay@users.noreply.github.com>
2021-03-10 19:05:45 -0500

    added js (still not complete)
    set up css styling for a hidden menu on mobile. need to fix js and figure out issue. (nav height is zero? is that right)

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
-
-console.log("Welcome to my crib");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
